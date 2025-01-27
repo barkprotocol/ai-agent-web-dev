7d1feee7c02cf3b25e07de225fb26f466754a93c
BARK Protocol <177503488+barkprotocol@users.noreply.github.com>
2025-01-27 15:34:39 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,2 +1,13 @@
-// This file is no longer needed and can be deleted
+import { PrismaClient } from '@prisma/client';
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
